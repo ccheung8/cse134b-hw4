31188d15ebe79156978ccdd82de3a551fbbea4f7
ccheung8 <c4cheung@ucsd.edu>
2023-03-01 09:34:51 -0800

    added hw3 files

---
+++ index.js
@@ -0,0 +1,11 @@
+// const canvas = document.getElementById("canvas");
+
+// const canvasCtx = canvas.getContext("2d");
+// canvasCtx.fillStyle = "yellow";
+// canvasCtx.fillRect(25, 50, 100, 100);
+
+// canvasCtx.fillStyle = "blue";
+// canvasCtx.fillRect(50, 75, 50, 50);
+
+// canvasCtx.fillStyle = "red";
+// canvasCtx.fillRect(62.5, 87.5, 25, 25);
